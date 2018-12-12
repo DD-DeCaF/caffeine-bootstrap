49370b61a4568d4033f8b43f5b81307565edfe0a
Ali Kaafarani <ali@kvikshaug.no>
2018-12-12 10:48:42 +0100

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+set -eu
+
+if [ $# != 1 ]; then
+
+  echo "Please read the README"
+  exit 1
+
+elif [ "$1" = "check" ]; then
+
+  [ "" != "$(which git)" ] && echo "git: ok"
+  [ "" != "$(which docker)" ] && echo "docker: ok"
+  [ "" != "$(which docker-compose)" ] && echo "docker-compose: ok"
+  [ "" != "$(which node)" ] && echo "node: ok"
+  [ "" != "$(which npm)" ] && echo "npm: ok"
+
+  if [ -f "modeling-base/cplex_128.tar.gz" ]; then
+    echo "modeling-base/cplex_128.tar.gz: ok"
+  else
+    echo "modeling-base/cplex_128.tar.gz: not found"
+  fi
+
+elif [ "$1" = "init" ]; then
+
+  echo "Welcome! This init script will perform the following actions:"
+  echo
+  echo "  * clone git repositories for all required services"
+  echo "  * install npm packages"
+  echo "  * build docker images"
+  echo "  * create empty local databases"
+  echo "  * create a demo user"
+  echo
+  echo "This may take a while (expect 10-15 minutes), depending on your bandwidth and processing power."
+  echo "Please make sure './bootstrap.sh check' is all ok before starting."
+  echo
+  echo "Press enter to start, or ctrl+c to abort."
+  read
+  
+  echo
+  echo "caffeine (1/8): cloning git repositories"
+  NPM_SERVICES="caffeine"
+  DOCKER_SERVICES="iam map-storage metabolic-ninja model model-storage warehouse"
+  SERVICES="${NPM_SERVICES} ${DOCKER_SERVICES}"
+  for SERVICE in ${SERVICES}; do
+    git clone https://github.com/dd-decaf/${SERVICE}
+  done
+
+  echo
+  echo "caffeine (2/8): installing npm packages"
+  pushd caffeine
+  npm install
+  popd
+
+  echo
+  echo "caffeine (3/8): building frontend distribution"
+  cp -v environment.ts caffeine/src/environments/
+  cp -v environment.staging.ts caffeine/src/environments/
+  pushd caffeine
+  npm run build
+  popd
+
+  echo
+  echo "caffeine (4/8): building modeling base image"
+  docker build -t gcr.io/dd-decaf-cfbf6/modeling-base:master modeling-base
+
+  echo
+  echo "caffeine (5/8): building services (this will take a few minutes)"
+  docker-compose build
+
+  echo
+  echo "caffeine (6/8): generating iam keys"
+  docker-compose run --rm iam ssh-keygen -t rsa -b 2048 -f keys/rsa -N ""
+  
+  echo
+  echo "caffeine (7/8): creating databases"
+  docker-compose up -d postgres
+	./iam/scripts/wait_for_postgres.sh
+	docker-compose exec postgres psql -U postgres -c "create database iam;"
+	docker-compose exec postgres psql -U postgres -c "create database maps;"
+	docker-compose exec postgres psql -U postgres -c "create database metabolic_ninja;"
+	docker-compose exec postgres psql -U postgres -c "create database model_storage;"
+	docker-compose exec postgres psql -U postgres -c "create database warehouse;"
+  docker-compose run --rm iam flask db upgrade
+  docker-compose run --rm map-storage flask db upgrade
+  docker-compose run --rm metabolic-ninja flask db upgrade
+  docker-compose run --rm model-storage flask db upgrade
+  docker-compose run --rm warehouse flask db upgrade
+  docker-compose stop
+
+  echo
+  echo "caffeine (8/8): creating demo user"
+  docker-compose run --rm iam python -c "from iam.models import User, db; from iam.app import app, init_app; init_app(app, db); app.app_context().push(); user = User(email='demo@demo'); user.set_password('demo'); db.session.add(user); db.session.commit()"
+
+  echo
+  echo "All done!"
+  echo "You may authenticate on the platform with email 'demo@demo' and password 'demo'."
+
+fi
---
+++ environment.staging.ts
@@ -0,0 +1,46 @@
+// Copyright 2018 Novo Nordisk Foundation Center for Biosustainability, DTU.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {Configuration} from './configuration';
+
+export const environment: Configuration = {
+  production: true,
+  apis: {
+    iam: 'http://localhost:8001',
+    model: 'http://localhost:8004',
+    model_storage: 'http://localhost:8005',
+    bigg: 'https://api.dd-decaf.eu/bigg',
+    map: 'http://localhost:8002',
+    metabolic_ninja: 'http://localhost:8003',
+    warehouse: 'http://localhost:8006',
+    maps: 'http://localhost:8002',
+  },
+  GA: {
+    trackingID: 'UA-106144097-3',
+  },
+  sentry: {
+    DSN: 'https://4ae40dd008994d91bc5632437fd5c395@sentry.io/1233153',
+    release: 'SENTRY_PROJECT',
+  },
+  firebase: {
+    api_key: 'AIzaSyApbLMKp7TprhjH75lpcmJs514uI11fEIo',
+    auth_domain: 'dd-decaf-cfbf6.firebaseapp.com',
+    database_url: 'https://dd-decaf-cfbf6.firebaseio.com',
+    project_id: 'dd-decaf-cfbf6',
+    storage_bucket: 'dd-decaf-cfbf6.appspot.com',
+    sender_id: '972933293195',
+  },
+  trustedURLs: ['https://api-staging.dd-decaf.eu/iam', 'https://api-staging.dd-decaf.eu/mstorage', 'https://api-staging.dd-decaf.eu/mcaffeine',
+    'https://api-staging.dd-decaf.eu/pathways-caffeine', 'https://api-staging.dd-decaf.eu/warehouse', 'https://api-staging.dd-decaf.eu/maps'],
+};
